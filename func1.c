b45e84375d44e788f063f755f24c1e59da538294
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-10-17 17:19:11 +0200

    delete_updates

--- func1.c
+++ func1.c
@@ -91,17 +91,3 @@ int print_int(int i)
 
 	return (count);
 }
-
-int print_unsigned(va_list ap, params_t *params)
-{
-	unsigned long l;
-
-	if (params->l_modifier)
-		l = (unsigned long)va_arg(ap, unsigned long);
-	else if (params->h_modifier)
-		l = (unsigned short int)va_arg(ap, unsigned int);
-	else
-		l = (unsigned int)va_arg(ap, unsigned int);
-	params->unsign = 1;
-	return (print_numbers(convert(l, 10, CONVERT_UNSIGNED, params), params));
-}
--- main.h
+++ main.h
@@ -8,18 +8,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define OUTPUT_BUF_SIZE 1024
-#define BUF_FLUSH -1
-
-#define FIELD_BUF_SIZE 50
-
-#define NULL_STRING "(null)"
-
-#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-
-#define CONVERT_LOWERCASE	1
-#define CONVERT_UNSIGNED	2
-
 /*STRUCTURE*/
 
 /**
@@ -34,23 +22,6 @@ typedef struct pick
 
 } pick_t;
 
-typedef struct parameters
-{
-	unsigned int unsign			: 1;
-
-	unsigned int plus_flag		: 1;
-	unsigned int space_flag		: 1;
-	unsigned int hashtag_flag	: 1;
-	unsigned int zero_flag		: 1;
-	unsigned int minus_flag		: 1;
-
-	unsigned int width;
-	unsigned int precision;
-
-	unsigned int h_modifier		: 1;
-	unsigned int l_modifier		: 1;
-} params_t;
-
 /*PROTOTYPE TO WRITE OUTPUT TO STDOUT*/
 int _putchar(char c);
 
@@ -62,5 +33,4 @@ int print_string(va_list ptr); /*to print string*/
 int print_char(va_list ptr); /* to print a single character*/
 int print_numbers(va_list arp);
 int print_int(int i);
-int print_unsigned(va_list ap, params_t *params);
 #endif
