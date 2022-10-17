35c8e049f693660b5cc71149086e7841383c5914
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-10-17 17:15:55 +0200

    main.h

--- func1.c
+++ func1.c
@@ -103,5 +103,5 @@ int print_unsigned(va_list ap, params_t *params)
 	else
 		l = (unsigned int)va_arg(ap, unsigned int);
 	params->unsign = 1;
-	return (print_number(convert(l, 10, CONVERT_UNSIGNED, params), params));
+	return (print_numbers(convert(l, 10, CONVERT_UNSIGNED, params), params));
 }
--- main.h
+++ main.h
@@ -8,6 +8,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#define FIELD_BUF_SIZE 50
+
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
 /*STRUCTURE*/
 
 /**
